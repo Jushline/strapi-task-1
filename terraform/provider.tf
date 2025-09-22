59c93999035275771086c8087b0b3283b839fdf4
Jushline Anitha <jushlineanitha@gmail.com>
2025-09-22 17:06:24 +0530

    Add terraform CD workflow and terraform

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,5 @@
 provider "aws" {
-  region = "ap-south-1"
+  region     = "ap-south-1" # or your region
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,10 @@ variable "docker_image" {
   description = "Docker image for Strapi"
   type        = string
 }
+variable "AWS_ACCESS_KEY_ID" {
+  type = string
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  type = string
+}
