fced5825980fecf5a5c3d2a8c2bde53fd8be2d78
Jushline Anitha <jushlineanitha@gmail.com>
2025-09-22 18:36:20 +0530

    modified network config

--- terraform/network.tf
+++ terraform/network.tf
@@ -10,6 +10,7 @@ resource "aws_vpc" "strapi_vpc" {
 resource "aws_subnet" "strapi_subnet" {
   vpc_id                  = aws_vpc.strapi_vpc.id
   cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-south-1a"   
   map_public_ip_on_launch = true
 
   tags = {
