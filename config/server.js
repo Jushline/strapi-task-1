35de466cb5630c59699bb0ba184f1c3ffcb6331c
Jushline <jushlineanitha2002@gmail.com>
2025-09-26 08:20:24 +0530

    strapi deployment

--- config/server.js
+++ config/server.js
@@ -1,11 +1,8 @@
-// config/admin.js
+// config/server.js
 module.exports = ({ env }) => ({
-  auth: {
-    // Admin JWT secret for admin login / sessions
-    secret: env('ADMIN_JWT_SECRET', 'default_admin_jwt_secret'),
-  },
-  apiToken: {
-    // API token salt required by Strapi v4
-    salt: env('API_TOKEN_SALT', 'default_api_token_salt'),
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  app: {
+    keys: env.array('APP_KEYS'), // will split by comma
   },
 });
