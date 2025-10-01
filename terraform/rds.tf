3e220bf8080345ab7db4012b33b5da046401b468
Jushline <jushlineanitha2002@gmail.com>
2025-10-01 09:09:48 +0530

    infra

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -7,20 +7,19 @@ resource "aws_db_subnet_group" "rds_subnets" {
 }
 
 resource "aws_db_instance" "postgres" {
-  identifier           = "${var.app_name}-db"
-  engine               = "postgres"
-  engine_version       = "15"
-  instance_class       = "db.t3.micro"
-  db_name              = var.database_name
-  username             = var.database_username
-  password             = var.database_password
-  allocated_storage    = 20
-  skip_final_snapshot  = true
-  publicly_accessible  = true
-  db_subnet_group_name = aws_db_subnet_group.rds_subnets.name
-  vpc_security_group_ids = [aws_security_group.ecs_sg.id]
-  port                 = var.database_port
-
+  identifier              = "${var.app_name}-db"
+  engine                  = "postgres"
+  engine_version          = "15"
+  instance_class          = "db.t3.micro"
+  db_name                 = var.database_name
+  username                = var.database_username
+  password                = var.database_password
+  allocated_storage       = 20
+  skip_final_snapshot     = true
+  publicly_accessible     = true
+  db_subnet_group_name    = aws_db_subnet_group.rds_subnets.name
+  vpc_security_group_ids  = [aws_security_group.ecs_sg.id]
+  port                    = var.database_port
   tags = {
     Name = "${var.app_name}-postgres"
   }
